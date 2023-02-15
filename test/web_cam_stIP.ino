4ebc43078c4bddfae6815215778cd6868ccfdcf7
DESKTOP-JENQPKF\Lenovo <lidongleibj@yuanfudao.com>
2023-02-15 18:57:02 +0800

    固定ip测试成功

--- test/web_cam_stIP.ino
+++ test/web_cam_stIP.ino
@@ -1,3 +1,4 @@
+// 正常运行led闪烁
 #include "battery.h"
 #include "esp_camera.h"
 #include <WiFi.h>
@@ -6,14 +7,15 @@
 
 #include "camera_pins.h"
 
-const char *ssid     = "DLGG_OW";
+const char *ssid = "DLGG_OW";
 const char *password = "makerdiy";
 
 void startCameraServer();
 
-void setup() {
+void setup()
+{
     Serial.begin(115200);
-    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0);  // disable   detector
+    WRITE_PERI_REG(RTC_CNTL_BROWN_OUT_REG, 0); // disable   detector
     bat_init();
     bat_hold_output();
     Serial.setDebugOutput(true);
@@ -23,56 +25,57 @@ void setup() {
 
     camera_config_t config;
     config.ledc_channel = LEDC_CHANNEL_0;
-    config.ledc_timer   = LEDC_TIMER_0;
-    config.pin_d0       = Y2_GPIO_NUM;
-    config.pin_d1       = Y3_GPIO_NUM;
-    config.pin_d2       = Y4_GPIO_NUM;
-    config.pin_d3       = Y5_GPIO_NUM;
-    config.pin_d4       = Y6_GPIO_NUM;
-    config.pin_d5       = Y7_GPIO_NUM;
-    config.pin_d6       = Y8_GPIO_NUM;
-    config.pin_d7       = Y9_GPIO_NUM;
-    config.pin_xclk     = XCLK_GPIO_NUM;
-    config.pin_pclk     = PCLK_GPIO_NUM;
-    config.pin_vsync    = VSYNC_GPIO_NUM;
-    config.pin_href     = HREF_GPIO_NUM;
+    config.ledc_timer = LEDC_TIMER_0;
+    config.pin_d0 = Y2_GPIO_NUM;
+    config.pin_d1 = Y3_GPIO_NUM;
+    config.pin_d2 = Y4_GPIO_NUM;
+    config.pin_d3 = Y5_GPIO_NUM;
+    config.pin_d4 = Y6_GPIO_NUM;
+    config.pin_d5 = Y7_GPIO_NUM;
+    config.pin_d6 = Y8_GPIO_NUM;
+    config.pin_d7 = Y9_GPIO_NUM;
+    config.pin_xclk = XCLK_GPIO_NUM;
+    config.pin_pclk = PCLK_GPIO_NUM;
+    config.pin_vsync = VSYNC_GPIO_NUM;
+    config.pin_href = HREF_GPIO_NUM;
     config.pin_sscb_sda = SIOD_GPIO_NUM;
     config.pin_sscb_scl = SIOC_GPIO_NUM;
-    config.pin_pwdn     = PWDN_GPIO_NUM;
-    config.pin_reset    = RESET_GPIO_NUM;
+    config.pin_pwdn = PWDN_GPIO_NUM;
+    config.pin_reset = RESET_GPIO_NUM;
     config.xclk_freq_hz = 20000000;
     config.pixel_format = PIXFORMAT_JPEG;
-    config.frame_size   = FRAMESIZE_UXGA;
+    config.frame_size = FRAMESIZE_UXGA;
     config.jpeg_quality = 10;
-    config.fb_count     = 2;
+    config.fb_count = 2;
 
     // camera init
     esp_err_t err = esp_camera_init(&config);
-    if (err != ESP_OK) {
+    if (err != ESP_OK)
+    {
         Serial.printf("Camera init failed with error 0x%x", err);
         return;
     }
 
     sensor_t *s = esp_camera_sensor_get();
     // initial sensors are flipped vertically and colors are a bit saturated
-    s->set_vflip(s, 1);        // flip it back
-    s->set_brightness(s, 1);   // up the blightness just a bit
-    s->set_saturation(s, -2);  // lower the saturation
+    s->set_vflip(s, 1);       // flip it back
+    s->set_brightness(s, 1);  // up the blightness just a bit
+    s->set_saturation(s, -2); // lower the saturation
 
     // drop down frame size for higher initial frame rate
     s->set_framesize(s, FRAMESIZE_QVGA);
 
     Serial.printf("Connect to %s, %s\r\n", ssid, password);
 
+    IPAddress staticIP(192, 168, 1, 206);
+    IPAddress gateway(192, 168, 1, 1);
+    IPAddress subnet(255, 255, 255, 0);
+    WiFi.config(staticIP, gateway, subnet);
 
-    IPAddress staticIP(192,168,254,147);
-IPAddress gateway(192,168,254,1);
-IPAddress subnet(255,255,255,0);
-
-    WiFi.config(mylocalIp, gateway, subnet);
     WiFi.begin(ssid, password);
 
-    while (WiFi.status() != WL_CONNECTED) {
+    while (WiFi.status() != WL_CONNECTED)
+    {
         delay(500);
         Serial.print(".");
     }
@@ -92,7 +95,8 @@ IPAddress subnet(255,255,255,0);
     Serial.println("' to connect");
 }
 
-void loop() {
+void loop()
+{
     // put your main code here, to run repeatedly:
     delay(100);
     digitalWrite(2, HIGH);
